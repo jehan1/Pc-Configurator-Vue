1fdc3ad427b9a147b66559c8f8208115966a9cdb
jehan <jehan.nilash@hotmail.com>
2019-04-06 01:46:23 +0100

    Updated main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+/* fontawesome */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faMinus, faPlus)
+
+Vue.component('fa-icon', FontAwesomeIcon)
+/*   */
+
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 

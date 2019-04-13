b5b139e36d9c47cf24b50a5ba9f66ffe94e3df59
jehan <jehan.nilash@hotmail.com>
2019-04-13 07:47:28 +0100

    Created Case.vue

---
+++ src/components/Case.vue
@@ -0,0 +1,36 @@
+<template>
+      <div id="CaseDropdown">
+      <b-dropdown variant="outline-success" v-if=" selectedSt.length >= 1 "
+                id="dropdown-1" :text="currentlySelectedCase.name" class="m-md-2">
+      <b-dropdown-item v-for=" c in Case" v-bind:key="c.id"
+                       v-on:click="handleCaseClick(c)">
+        {{ c.name}}
+      </b-dropdown-item>
+    </b-dropdown>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Case',
+
+    props:{
+        Case: Array,
+        selectedSt: Array
+    },
+
+    data(){
+        return{
+           currentlySelectedCase: {name:'Select The Case...'}
+        }
+    },
+
+    methods:{
+        handleCaseClick(c){
+            this.currentlySelectedCase = c
+            this.$emit('handle-case-click',c)
+        }
+    }
+
+}
+</script>

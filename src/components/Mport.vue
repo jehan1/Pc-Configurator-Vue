eaf2b778f827004d4673e3180e6d8849305fdc90
jehan <jehan.nilash@hotmail.com>
2019-04-13 07:26:54 +0100

    Created Mport.vue

---
+++ src/components/Mport.vue
@@ -0,0 +1,59 @@
+<template>
+     <div id="mPortDropdown">
+     <b-dropdown variant="outline-success" v-if="this.selectedSt.length >= 1 "
+                id="dropdown-1" text="Select M.2 Drive..." class="m-md-2" :disabled="this.mPorts.length == 0">
+      <b-dropdown-item v-for="mp in mPorts" v-bind:key="mp.id"
+                       v-on:click="handleMportClick(mp)">
+        {{ mp.name }}
+      </b-dropdown-item>
+    </b-dropdown>
+  
+    <b-list-group>
+      <b-list-group-item v-for="sMports in selectedMports" v-bind:key="sMports.id" >
+        <div class="selected-mPort-item">
+          <p id="selected-mPort-name">{{ sMports.name }}</p>
+          <p id="selected-mPort-quantity">Quantity: {{ sMports.quantity }}</p>
+          <b-button class="selected-mPort-btn" @click="increaseMportQuantity(sMports)" variant="success">
+            <fa-icon icon="plus"></fa-icon>
+          </b-button>
+          <b-button class="selected-mPort-btn" @click="decreaseMportQuantity(sMports)" variant="warning">
+            <fa-icon icon="minus"></fa-icon>
+          </b-button>
+          <b-button class="selected-mPort-btn" @click="removeSelectedMport(sMports)" variant="outline-danger">Remove</b-button>
+        </div>
+      </b-list-group-item>
+    </b-list-group>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Mport',
+
+    props:{
+        mPorts: Array,
+        selectedMports: Array,
+        selectedSt: Array
+
+    },
+
+    methods:{
+        handleMportClick(mp){
+            this.$emit('handle-mport-click',mp)
+        },
+
+        increaseMportQuantity(sMports){
+            this.$emit('increase-mport-quantity',sMports)
+        },
+
+        decreaseMportQuantity(sMports){
+            this.$emit('decrease-mport-quantity',sMports)
+        },
+
+        removeSelectedMport(sMports){
+            this.$emit('remove-selected-mport',sMports)
+        }
+
+    }
+}
+</script>

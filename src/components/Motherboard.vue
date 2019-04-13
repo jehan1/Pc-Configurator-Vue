c80402fe97087ede6c1dcecefe13d928b76ddfde
jehan <jehan.nilash@hotmail.com>
2019-04-13 02:20:47 +0100

    Created Motherboard.vue
    Modularising the project

---
+++ src/components/Motherboard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <b-dropdown id="dropdown-1" variant="outline-primary" :text="currentlySelectedMotherboard.name" class="m-md-2">
+      <b-dropdown-item v-for="mobo in motherboard" v-bind:key="mobo.id"
+                      v-on:click="handleMoboClick(mobo)">
+        {{ mobo.name }}
+      </b-dropdown-item>
+      </b-dropdown>
+    </div>
+</template>
+
+<script>
+import { config } from '../main.js';
+
+export default {
+    name: 'Motherboard',
+    props: ["motherboard"],
+
+    data(){
+        return{
+            currentlySelectedMotherboard: { name: 'Select motherboard...'},
+        }
+    },
+    methods:{
+        handleMoboClick(mobo){
+            this.currentlySelectedMotherboard = mobo
+            this.$emit('handle-mobo-click',mobo);
+            config.$emit('handle-mobo-click',this.currentlySelectedMotherboard);
+        }
+    }
+}
+
+</script>
+

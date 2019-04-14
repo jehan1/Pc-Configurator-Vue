0549f4ff4f8c5f1a97d131a92dcba6229cf69b6a
jehan <jehan.nilash@hotmail.com>
2019-04-14 03:42:49 +0100

    Created Submit button

---
+++ src/components/Submit.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id= "submitButton" >
+       
+    <b-button 
+        variant="primary"
+        v-if="selectedPowerSupply.id != null"
+        >
+        submit
+        </b-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Submit',
+
+    props:{
+        selectedPowerSupply: Object,
+
+    }
+}
+</script>
+
+<style scoped>
+
+#submitButton{
+    margin: 20px 0px 0px 0px;
+}
+
+</style>
+
+
+
